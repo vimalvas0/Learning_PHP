a7fdd1e01ec89f40199c873b84ae9122172be1b4
vimalvas0 <vimalvas03@gmail.com>
2021-04-05 20:04:33 +0530

    Improving indentation

--- strings.php
+++ strings.php
@@ -1,34 +1,34 @@
 <?php 
 
 
-			// Let's start with the simple string
+	// Let's start with the simple string
 
-			$string = 'Hello, Shubhi';
-			$string2 = 'How are you';
-
-
-			$string3 = "$string, $string2?";
-			echo "$string3 \n" . '<br>';
-			echo "This should be another line...." . '<br>';
-			
+	$string = 'Hello, ';
+	$string2 = 'How are you';
 
 
-			// Strings methods : 
-			//
-
-			echo "String length of string will be " . " ";
-			echo strlen($string) . '<br>';
-
-
-			$string4 = "    Hello Guys    " . PHP_EOL;
-			echo "Untrimmed string " . " : $string4". '<br>';
-			echo "Trimmed string : " . trim($string4)  .  '<br>';
+	$string3 = "$string, $string2?";
+	echo "$string3 \n" . '<br>';
+	echo "This should be another line...." . '<br>';
+	
 
 
-			$string5  = "My name is vimal vasisth". '<br>';
-			echo sha1($string5) . '<br>';
+	// Strings methods : 
+	//
+
+	echo "String length of string will be " . " ";
+	echo strlen($string) . '<br>';
 
 
-			echo levenshtein($string4, $string5);	
-			
-		?>
+	$string4 = "    Hello Guys    " . PHP_EOL;
+	echo "Untrimmed string " . " : $string4". '<br>';
+	echo "Trimmed string : " . trim($string4)  .  '<br>';
+
+
+	$string5  = "My name is vimal vasisth". '<br>';
+	echo sha1($string5) . '<br>';
+
+
+	echo levenshtein($string4, $string5);	
+	
+?>
